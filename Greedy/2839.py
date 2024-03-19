9c27a70956bc338c4e5d3c1d900823d728899472
Jina Choi <cjina1102@gmail.com>
2024-03-19 10:44:13 +0900

    11399.py greedy (sliver4)

--- Greedy/2839.py
+++ Greedy/2839.py
@@ -1,27 +1,25 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-
 # 3kg, 5kg 봉지 있음 -> 5는 3의 배수가 아닌데, 그럼 DP로 해야하는건가?
 
+
+n = int(input())
 cnt = 0
 
 while n>=0:
-    if n%5 == 0: # n이 5의 배수라면
+    if n%5 == 0:
         cnt += n//5
         n = 0
         break
         
-    n -= 3 # n이 5의 배수가 아닌 경우
-    cnt += 1
+    n -= 3
+    cnt += 1  # 5의 배수가 될 때까지 설탕-3, 봉지+1 (5의 배수가 될때까지 3을 빼준다는 것이 이 문제의 아이디어)
     
 if n==0:
     print(cnt)
 else:
-    print(-1) # 정확하게 N킬로그램을 만들 수 없다면 -1을 출력
-
-
+    print(-1)
 '''
 bag = 0
 check = True
