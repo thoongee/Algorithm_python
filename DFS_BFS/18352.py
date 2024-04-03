3b411ab112c5638e76aeade1377d2bc97ca7a8d5
Jina Choi <cjina1102@gmail.com>
2024-04-04 00:07:46 +0900

    18352.py <DFS/BFS> sliver2

--- DFS_BFS/18352.py
+++ DFS_BFS/18352.py
@@ -1,28 +1,29 @@
 import sys
+from collections import deque
+input = sys.stdin.readline
 
 n,m,k,x = map(int,input().strip().split(' '))
 
-graph = [[] for _ in range(n+1)] # **이중 리스트 초기화**
+graph = [[] for _ in range(n+1)] # 이중 리스트 초기화 
 for i in range(m):
     node, road = map(int,input().strip().split(' '))
     graph[node].append(road)
 
-cities = [1000000]*(n+1) # m의 최대값이 1000000이라서.
-cities[0] = 0
-cities[x] = 0  # 0번째와 x번째 노드는 거리가 0
+distance = [-1]*(n+1)
+distance[x] = 0
 
-def dfs(graph, v):
-# 현재 노드(v)와 연결된 다른 노드를 재귀적으로 방문
-    for i in graph[v]:
-        new_cities = cities[v]+1
-        cities[i] = min(new_cities,cities[i])
-        dfs(graph, i)
+queue = deque()
+queue.append(x)
+while queue:
+    i = queue.popleft()
+    for v in graph[i]:
+        if distance[v] == -1:  # 아직 방문하지 않은 노드에 대해서만 처리
+            distance[v] = distance[i] + 1
+            queue.append(v)  # 방문하지 않은 노드만 큐에 추가
 
-#정의된 DFS 함수 호출
-dfs(graph, x)
 
 # 최단 거리가 K인 모든 도시의 번호를 한 줄에 하나씩 오름차순으로 출력
-indexes_of_k = [i for i, value in enumerate(cities) if value == k]
+indexes_of_k = [i for i, value in enumerate(distance) if value == k]
 indexes_of_k.sort()
 
 if len(indexes_of_k)==0:
