e9645b63e41660dd52fa899eafb32df555dbb17b
Jina Choi <cjina1102@gmail.com>
2024-04-07 12:32:04 +0900

    10825 <sorting> sliver4

--- Sorting/10825.py
+++ Sorting/10825.py
@@ -5,7 +5,7 @@ n = int(input().strip())
 students = [] # 학생 정보를 담을 리스트
 # 모든 학생 정보를 입력받기
 for _ in range(n):
- students.append(input().strip().split())
+    students.append(input().strip().split())
 
 '''
 [정렬 기준]
@@ -18,4 +18,4 @@ students.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
 
 # 정렬된 학생 정보에서 이름만 출력
 for student in students:
- print(student[0])
+    print(student[0])
