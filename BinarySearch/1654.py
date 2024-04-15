870afff212e61467ad7def24af4ee9a1f9aa4d8a
Jina Choi <cjina1102@gmail.com>
2024-04-15 21:04:41 +0900

    1654 <binary search> sliver2

---
+++ BinarySearch/1654.py
@@ -0,0 +1,18 @@
+import sys
+k,n = map(int, input().split())
+lan = [int(sys.stdin.readline()) for _ in range(k)]
+start = 1
+end = max(lan)
+
+while start <= end: # start > end 가 되기 전까지만 실행됌. 
+    mid = (start + end) // 2 
+    cnt = 0 # 랜선 수
+    for i in lan:
+        cnt += i // mid # mid 길이로 각 랜선을 잘랐을 때, 생기는 랜선들의 총 갯수
+        
+    if cnt >= n:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(end)
