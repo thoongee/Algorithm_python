0d2b6969576e96ca8ae260ef681f8b92755d0b3c
Jina Choi <cjina1102@gmail.com>
2024-04-20 22:26:26 +0900

    1920 <binary search> sliver4

--- BinarySearch/1920.py
+++ BinarySearch/1920.py
@@ -1,19 +1,44 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input().rstrip())
-n_set = set(input().rstrip().split())
+def binary_search(arr, x):
+    start = 0
+    end = len(arr) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] < x:
+            start = mid + 1
+        elif arr[mid] > x:
+            end = mid - 1
+        else: # arr[mid] == x
+            return True
+    return False
 
-m = int(input().rstrip())
-m_li = input().rstrip().split()
-check = []
+n = int(input().strip())
+n_li = sorted(list(map(int, input().strip().split())))
+m = int(input().strip())
+m_li = list(map(int, input().strip().split()))
 
-# set, dictionary의 in연산을 통한 포함 여부 확인 : O(1)
-# 값이 존재하는지 여부만 알면 됌 --> a를 set 자료형으로 변경
-# 굳이 int자료형일 필요x
+for m in m_li:
+    if binary_search(n_li, m):
+        print(1)
+    else:
+        print(0)
 
-for i in m_li: #O(n)
-  if i in n_set:
-    print(1)
-  else:
-    print(0)
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input().rstrip())
+# n_set = set(input().rstrip().split()) # 굳이 int자료형일 필요x
+# print(n_set)
+# m = int(input().rstrip())
+# m_li = input().rstrip().split() # 굳이 int자료형일 필요x
+
+# # set, dictionary의 in연산을 통한 포함 여부 확인 : O(1)
+# # 값이 존재하는지 여부만 알면 됌 --> a를 set 자료형으로 변경
+
+# for i in m_li: #O(n)
+#   if i in n_set:
+#     print(1)
+#   else:
+#     print(0)
