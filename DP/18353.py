7c11153fa227b82c41e0d4fd34b99628cd60a64c
Jina Choi <cjina1102@gmail.com>
2024-04-29 16:31:54 +0900

    modify : 18353 <dp> sliver2

--- DP/18353.py
+++ DP/18353.py
@@ -3,6 +3,8 @@ input = sys.stdin.readline
 
 n = int(input().strip())
 array = list(map(int, input().split()))
+
+# 주어진 문제 : 가장 긴 감소하는 부분 수열’의 길이를 계산하는 문제
 # 순서를 뒤집어 '가장 긴 증가하는 부분 수열' 문제로 변환
 array.reverse()
 
@@ -15,5 +17,5 @@ for i in range(1, n):
         if array[j] < array[i]:
             dp[i] = max(dp[i], dp[j] + 1)
 
-# 열외시켜야 하는 병사의 최소 수
+# 열외시켜야 하는 병사의 최소 수 = 초기 병사 수 - LIS 길이
 print(n - max(dp))
