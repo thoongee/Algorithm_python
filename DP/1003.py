f15f5df819149eb9ff7844b3725fc267e9efa4dc
Jina Choi <cjina1102@gmail.com>
2024-04-29 16:06:51 +0900

    18353, 1932 <dp> sliver2,1

--- DP/1003.py
+++ DP/1003.py
@@ -1,1 +1,15 @@
-import sys
+# 손으로 그려서 규칙을 찾음 (n,cnt_0,cnt_1의 갯수)
+# 0의 갯수 = 이전 1의 개수
+# 1의 개수 = 이전 0+1의 개수
+t = int(input())
+ 
+for _ in range(t):
+  cnt_0 = [1,0]
+  cnt_1 = [0,1]
+  n = int(input())
+  if n>1:
+    for i in range(n-1):
+      cnt_0.append(cnt_1[-1])
+      cnt_1.append(cnt_0[-2]+cnt_1[-1]) 
+ 
+  print(cnt_0[n], cnt_1[n])
--- DP/1463.py
+++ DP/1463.py
@@ -1,2 +1,21 @@
 import sys
-input = sys.stdin.readin()
+input = sys.stdin.readline
+
+n = int(input().strip())
+
+# 앞서 계산된 결과를 저장하기 위한 DP 테이블 초기화
+# index : 수
+# value : 함수 호출 횟수
+d = [0] * (n+1)
+
+# bottom-up
+for i in range(2, n+1):
+    # 현재의 수에서 1을 빼는 경우
+    d[i] = d[i - 1] + 1
+    # 현재의 수가 3으로 나누어 떨어지는 경우
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i // 3] + 1)
+    # 현재의 수가 2로 나누어 떨어지는 경우
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i // 2] + 1)
+print(d[n])
