6794e0b67aa86e4a0d140e4d1720237a3ae759e9
Jina Choi <cjina1102@gmail.com>
2024-05-03 23:33:47 +0900

    2875 <greedy> bronze3

---
+++ Greedy/2875.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n, m, k = map(int, input().strip().split())
+team = 0  # 팀 수
+
+while n >= 2 and m >= 1 and n + m >= k + 3:  # 2명의 여학생과 1명의 남학생이 필요하고, 인턴쉽에 참여하지 않을 충분한 인원이 남아있어야 함
+  team += 1
+  n -= 2
+  m -= 1
+
+print(team)
